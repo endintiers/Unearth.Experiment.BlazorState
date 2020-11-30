87aee7becd97442bcd0cf1d8c3454e1890e8278c
Chris Hewitt <chris.hewitt@wildmouse.com>
2020-11-30 11:38:26 +1100

    Add project files.

---
+++ Unearth.Experiment.BlazorStateNoAuth/Services/ScopedState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unearth.Experiment.BlazorStateNoAuth.Services
+{
+    public class ScopedState
+    {
+        public Guid Guid { get; set; }
+        public int CurrentCount { get; set; }
+        public ScopedState()
+        {
+            Guid = Guid.NewGuid();
+            Debug.WriteLine($"Guid: {Guid}");
+            CurrentCount = 0;
+        }
+    }
+}
